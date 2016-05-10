11c2b30624c084b80c8af5d101a59252e8433182
Łukasz <draganl@student.mi.pw.edu.pl>
2016-05-10 21:31:09 +0200

    zrównoleglenie ewolucji

--- src/msi/Program.java
+++ src/msi/Program.java
@@ -37,7 +37,7 @@ public class Program {
         //FitnessCalculator.setSolution("1111000000000000000000000000000000000000000000000000000000001111");
 		IFitness fitnessCalc = new TestEvaluator();// new FitnessCalculator();
         // Create an initial population
-        Population myPop = new Population(20, true, fitnessCalc);
+        Population myPop = new Population(10, true, fitnessCalc);
         ITournament tournament = new Tournament();
         Algorithm alg = new Algorithm(tournament);
         // Evolve our population until we reach an optimum solution
@@ -45,14 +45,14 @@ public class Program {
         while (generationCount<10000)//(myPop.getFittest().getFitness(fitnessCalc) < FitnessCalculator.getMaxFitness(fitnessCalc))
         {
             generationCount++;
-            System.out.println("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness(fitnessCalc));
-            System.out.println(myPop.getFittest());
+            //System.out.println("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness(fitnessCalc));
+            //System.out.println(myPop.getFittest());
             myPop = alg.evolvePopulation(myPop);
         }
         System.out.println("Solution found!");
         System.out.println("Generation: " + generationCount);
         System.out.println("Genes:");
-        System.out.println(myPop.getFittest());
+        //System.out.println(myPop.getFittest());
 	}
 
 }
